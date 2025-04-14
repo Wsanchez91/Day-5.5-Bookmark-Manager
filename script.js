cac523f968fe832ae94d6d3656e78cd72f182b13
William Sanchez <williamsanchez.8055@gmail.com>
2025-04-14 15:39:30 -0400

    working on js file

--- script.js
+++ script.js
@@ -7,3 +7,58 @@ const devBtn = document.querySelector("#dev-btn");
 const newsBtn = document.querySelector("#news-btn");
 const videoBtn = document.querySelector("#video-btn");
 const bookList = document.querySelector("#book-list");
+
+const bookmarkData = JSON.parse(localStorage.getItem("bookmark")) || [];
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookmark();
+});
+
+const addBookmark = () => {
+  const nameValue = siteName.value.trim();
+  const urlValue = siteUrl.value;
+  const categoryValue = category.value;
+  const deleteBtn = document.querySelector("#delete-btn");
+
+  const bookmarkObj = {
+    name: nameValue,
+    url: urlValue,
+    category: categoryValue,
+  };
+
+//   const updateBookmarkList = () => {
+//     bookList.innerHTML = "";
+
+//     bookmarkData.forEach(({ name, url, category }) => {
+//       bookList.innerHTML += `
+//         <div>
+//           <p><strong>Name: </strong>${name}</p>
+//           <p><strong>URL: </strong>${url}</p>
+//           <p><strong>Category: </strong>${category}</p>
+//           <button onclick="deleteBtn()" id="delete-btn">Delete</button>
+//           </div>`;
+//     });
+//   };
+
+  bookList.innerHTML += `
+  <div>
+    <p><strong>Name: </strong>${bookmarkObj.name}</p>
+    <p><strong>URL: </strong>${bookmarkObj.url}</p>
+    <p><strong>Category: </strong>${bookmarkObj.category}</p>
+    <button onclick="deleteBtn()" id="delete-btn">Delete</button>
+    </div>`;
+
+
+  deleteBtn.addEventListener("click", () => {
+    p.remove();
+    updateStorage();
+  });
+
+  bookmarkData.push(bookmarkObj);
+  localStorage.setItem("bookmark", JSON.stringify(bookmarkData));
+
+//   updateBookmarkList();
+  console.log(bookmarkObj);
+  console.log(bookList);
+};
